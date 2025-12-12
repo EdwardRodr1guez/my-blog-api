e307f8d4e3c846ce475235154605c65434087da7
edward-ios <edward.rodriguez@pharitechservices.com>
2025-12-12 15:40:07 -0500

    fix(users): corrige error de sintaxis en users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  private users: User[] = [
+    { id: '1', name: 'Alice', email: 'alice@example.com' },
+    { id: '2', name: 'Bob', email: 'bob@example.com' },
+    { id: '3', name: 'Charlie', email: 'charlie@example.com' },
+    //mas usuarios...
+  ];
+}
